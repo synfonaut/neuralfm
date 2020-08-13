94d54c66cb417c3c993ba59a78bcba15adbcda93
synfonaut <synfonaut@protonmail.com>
2020-08-13 01:28:38 -0700

    added bsv scraper tests

--- plugins/scrapers/bsv.twitter.js
+++ plugins/scrapers/bsv.twitter.js
@@ -1,5 +1,42 @@
-function BSVTwitterScraper() {
+const config = require("../../config");
+
+const Twitter = require("twitter");
+
+function BSVTwitterScraper(opts={}) {
+  const limit = opts.limit || 10;
+  const username = opts.username;
+
+  return new Promise(function(resolve, reject) {
+    if (!username) {
+      return reject("invalid username");
+    }
+
+    const client = BSVTwitterScraper.getTwitterClient();
+
+    return [];
+  });
 }
+
+BSVTwitterScraper.getTwitterClient = function() {
+  return new Twitter(config.twitter);
+}
+
+BSVTwitterScraper.getTwitterAccounts = function() {
+  return ["synfonaut"];
+};
+
+BSVTwitterScraper.getRecentTweetsForTwitterAccount = function(client, username) {
+  return new Promise(function(resolve, reject) {
+    if (!client) { return reject("invalid client") }
+    if (!username) { return reject("invalid username") }
+
+    const params = { screen_name: username, trim_user: 1, count: 200 };
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+      if (error) { return reject(error) }
+      resolve(tweets);
+    });
+  });
+};
 
 /** Author: Enter the name of the creator of this plugin **/
 BSVTwitterScraper.author = "synfonaut";
--- plugins/scrapers/bsv.twitter.test.js
+++ plugins/scrapers/bsv.twitter.test.js
@@ -1,12 +1,13 @@
 const assert = require("assert");
 const plugins = require("../index");
 
+assert(plugins);
+assert(plugins.scrapers.BSVTwitterScraper);
+
+const BSVTwitterScraper = plugins.scrapers.BSVTwitterScraper;
+
 describe("bsv twitter scraper", function () {
   it("verify plugin meta data", function () {
-    assert(plugins);
-    assert(plugins.scrapers.BSVTwitterScraper);
-
-    const BSVTwitterScraper = plugins.scrapers.BSVTwitterScraper;
     assert.equal(BSVTwitterScraper.dataset, "BSV Twitter");
     assert.equal(BSVTwitterScraper.author, "synfonaut");
     assert.equal(BSVTwitterScraper.paymail, "synfonaut@moneybutton.com");
@@ -14,10 +15,20 @@ describe("bsv twitter scraper", function () {
     assert(BSVTwitterScraper.version);
   });
 
-  it.skip("gets recent tweets from user", function () {
-    const results = BSVTwitterScraper();
-    console.log("RESULTS");
-    assert(results);
+  it("gets recent tweets from user", function (done) {
+    this.timeout(10000);
+    this.slow(5000);
+    const client = BSVTwitterScraper.getTwitterClient();
+    BSVTwitterScraper.getRecentTweetsForTwitterAccount(client, "synfonaut").then(function (results) {
+      assert(results);
+      assert(results.length > 5);
+      assert(results[0].created_at);
+      assert(results[0].id);
+      assert(results[0].text);
+      assert(results[0].user);
+      assert(results[0].user.id);
+      done();
+    });
   });
 });
 
