7f65275d584cd1760a28a0eea17250a5d8a69a34
synfonaut <synfonaut@protonmail.com>
2020-08-16 14:26:08 -0700

    navbar finished

--- app/nav.jsx
+++ app/nav.jsx
@@ -4,12 +4,23 @@ import { Link } from "caldera"
 
 export function NavigationBar(args={}) {
 
+  const [isShowingMobileNavigation, setIsShowingMobileNavigation] = React.useState(false);
+  function handleToggleMobileNavigation() {
+    setIsShowingMobileNavigation(!isShowingMobileNavigation);
+  }
+
+  function hideMobileNavigation() {
+    setIsShowingMobileNavigation(false);
+  }
+
   function handleClickHomepage() {
     args.history.push("/");
+    hideMobileNavigation();
   }
 
   function handleClickCreate() {
     args.history.push("/create");
+    hideMobileNavigation();
   }
 
   return <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
@@ -19,22 +30,38 @@ export function NavigationBar(args={}) {
         <p className="subtitle">AI Information Radio</p>
       </div>
 
-      <div className="navbar-menu">
-        <a className="navbar-item" href="">Bitcoin</a>
-        <a className="navbar-item" href="">Business</a>
-        <a className="navbar-item" href="">Technology</a>
-        <a className="navbar-item" href="">Metanet</a>
-        <a className="navbar-item" href="">Proof of Work</a>
-        <a className="navbar-item" href="">Decentralized Applications</a>
-        <a className="navbar-item" href="">Wallets</a>
-        <a className="navbar-item" onClick={handleClickCreate}>Create</a>
-      </div>
-
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+      <a onClick={handleToggleMobileNavigation} role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
+
+    <div id="navbarBasicExample" className={"navbar-menu" + (isShowingMobileNavigation ? " is-active": "")}>
+      <div className="navbar-start">
+          <a className="navbar-item" href="">Bitcoin</a>
+          <a className="navbar-item" href="">Business</a>
+          <a className="navbar-item" href="">Technology</a>
+          <a className="navbar-item" href="">Metanet</a>
+          <a className="navbar-item" href="">Proof of Work</a>
+          <a className="navbar-item" href="">Decentralized Applications</a>
+          <a className="navbar-item" href="">Wallets</a>
+          <hr className="navbar-divider is-hidden-desktop" />
+          <a className="navbar-item is-hidden-desktop" onClick={handleClickCreate}>
+            Create
+          </a>
+        </div>
+      </div>
+
+      <div className="navbar-end is-hidden-mobile">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a className="button create-button" onClick={handleClickCreate}>
+              Create
+            </a>
+          </div>
+        </div>
+      </div>
   </nav>
 }
+
