76025a6b6cc686437f08f714f84a459fdec71d8c
synfonaut <synfonaut@protonmail.com>
2020-08-17 02:14:37 -0700

    wording

--- app/sidebar.jsx
+++ app/sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 export function Sidebar() {
   return <div id="sidebar">
-        <p className="content"><strong>NeuralFM</strong>'s mission is to put you in control of the AI's feeding you information.</p>
+        <p className="content"><strong>NeuralFM</strong> is on a mission is to put you in control of the AI's feeding you information.</p>
         <p className="content">Every channel on NeuralFM is an AI trained by a human editor, and every decision that human makes is logged permanently on the blockchain.</p>
         <p className="content">Create your own AI with the click of a button, or use of the open market AI's created by other humans.</p>
 
