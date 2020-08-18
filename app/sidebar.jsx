e8e1bc161c8a9bffb0784cd3dd8af191c1d67a0e
synfonaut <synfonaut@protonmail.com>
2020-08-18 07:25:13 -0700

    typo

--- app/sidebar.jsx
+++ app/sidebar.jsx
@@ -4,7 +4,7 @@ export function Sidebar() {
   return <div id="sidebar">
         <p className="content"><strong>NeuralFM</strong> is on a mission is to put you in control of the AI's feeding you information.</p>
         <p className="content">Every channel on NeuralFM is an AI trained by a human editor, and every decision that human makes is logged permanently on the blockchain.</p>
-        <p className="content">Create your own AI with the click of a button, or use of the open market AI's created by other humans.</p>
+        <p className="content">Create your own AI with the click of a button, or use the open market AI's created by other humans.</p>
 
         <p className="content">Stay goodbye to filter bubbles, disinformation and fake news. Say Hello to your personal news AI on Bitcoin.</p>
     </div>
